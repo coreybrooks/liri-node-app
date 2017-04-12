fd41cb860b432558d484833d9f1fae9a780dfc8b
Corey <coreybrooks2@gmail.com>
2017-04-11 19:21:38 -0500

    removing keys

--- keys.js
+++
@@ -1,9 +0,0 @@
-console.log('this is loaded');
-console.log('------------------');
-
-exports.twitterKeys = {
-  consumer_key: '1ztXypR4dfqJ0Mu8VqpEERuma',
-  consumer_secret: 'DiMrrkKLRbOFEu1i6WgAPAShNkyxkRKLlVo3u3t694ZqPGHWVk',
-  access_token_key: '762761932205985792-vx0NYebDPTf5pyjRf8dw0UFiX3wsa0r',
-  access_token_secret: 'UYhG71N7TExRm47vSlz4QATciZkpIz5anw5ZIdDe4bY1j',
-}
